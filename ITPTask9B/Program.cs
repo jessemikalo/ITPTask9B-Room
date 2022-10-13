9db3c144919e54e50a152ca76fbd8b9e1b7a2a26
jesse <jesse@JESSE>
2022-10-13 11:12:23 +0300

    Add project files.

---
+++ ITPTask9B/Program.cs
@@ -0,0 +1,47 @@
+﻿using ITPTask9B;
+using System;
+
+bool stillMore;
+double lenght, breadth, height; 
+
+do
+{
+    Console.Write("Input the lenght, please: ");
+    string received = Console.ReadLine();
+    //Should be of right data type and a positive value
+    while(!Double.TryParse(received, out lenght) || lenght < 0)
+    {
+        Console.Write("Not accepted, concentrate: ");
+        received = Console.ReadLine();
+    }
+
+    Console.Write("Input the breadth, please: ");
+    received = Console.ReadLine();
+    //Should be of right data type and a positive value
+    while (!Double.TryParse(received, out breadth) || breadth < 0) 
+    {
+        Console.Write("Not accepted, concentrate: ");
+        received = Console.ReadLine();
+    }
+
+    Console.Write("Input the height, please: ");
+    received = Console.ReadLine();
+    //Should be of right data type and a positive value
+    while (!Double.TryParse(received, out height) || height < 0) 
+    {
+        Console.Write("Not accepted, concentrate: ");
+        received = Console.ReadLine();
+    }
+    //Perfect information available:
+    Room_ room = new Room_(lenght, breadth, height);
+
+    Console.WriteLine("There area of the floor is {0:F3}.", room.CalculateFloorArea());
+    Console.WriteLine("The area of the walls is {0:F3}.", room.CalculateWallArea());
+
+    Console.Write("More of this (Y/N): ");
+    string decision = Console.ReadLine().ToUpper();
+    if (decision.StartsWith("Y)"))
+        stillMore = true;
+    else
+        stillMore = false;
+}while(stillMore);
---
+++ ITPTask9B/Room,.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITPTask9B
+{
+    internal class Room_
+    {
+        public Room_(double lenght, double breadth, double height)
+        {
+            Lenght = lenght;
+            Breadth = breadth;
+            Height = height;
+        }
+
+        public double Lenght { get; set; }
+        public double Breadth { get; set; }
+        double Height { get; set; }
+    
+
+        public double CalculateFloorArea()
+        {
+        return Lenght * Breadth;
+        }
+
+        public double CalculateWallArea()
+        {
+            return 2 * (Lenght * Height) + 2 * (Breadth * Height);
+        }
+
+    }
+}
